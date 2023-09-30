dd6a918f7dfc3ba0e38b53dba3ab65cf85f4a2bb
Simeon Widdis <sawiddis@gmail.com>
2023-09-30 13:01:00 -0700

    Add base check to search prioritization

--- v2/search/mod.rs
+++ v2/search/mod.rs
@@ -2,9 +2,10 @@ use std::cmp::max;
 
 use crate::{
     core::{
-        constants8::{BLACK_START, WHITE_START},
+        constants8::{BLACK_FIRST_ROW, BLACK_START, WHITE_FIRST_ROW, WHITE_START},
+        move8::BreakthroughMove,
         node8::BreakthroughNode,
-        Evaluation, GameResult, Player, move8::BreakthroughMove,
+        Evaluation, GameResult, Player,
     },
     evaluation::fast_heuristic,
 };
@@ -50,27 +51,42 @@ fn get_prioritized_actions(node: &BreakthroughNode) -> Vec<BreakthroughMove> {
 
     let actions = node.get_possible_actions();
 
-    let mut actions: Vec<BreakthroughMove> = actions.into_iter().filter(|action| {
-        // If opponent is threatening a win, we can only recapture
-        match node.to_play {
-            Player::White => if node.bitboard_black & WHITE_START == 0 {
-                true
-            } else {
-                (1 << action.1) & node.bitboard_black & WHITE_START > 0
-            },
-            Player::Black => if node.bitboard_white & BLACK_START == 0 {
-                true
-            } else {
-                (1 << action.1) & node.bitboard_white & BLACK_START > 0 
+    let mut actions: Vec<BreakthroughMove> = actions
+        .into_iter()
+        .filter(|action| {
+            // If opponent is threatening a win, we can only recapture
+            match node.to_play {
+                Player::White => {
+                    if node.bitboard_black & WHITE_START == 0 {
+                        true
+                    } else {
+                        (1 << action.1) & node.bitboard_black & WHITE_START > 0
+                    }
+                }
+                Player::Black => {
+                    if node.bitboard_white & BLACK_START == 0 {
+                        true
+                    } else {
+                        (1 << action.1) & node.bitboard_white & BLACK_START > 0
+                    }
+                }
             }
-        }
-    }).collect();
+        })
+        .collect();
 
     actions.sort_unstable_by_key(|action| {
         let target_square = 1 << action.1;
-        let (opp_start, opp_side) = match node.to_play {
-            Player::White => (BLACK_START, BLACK_START | (BLACK_START << 16)),
-            Player::Black => (WHITE_START, WHITE_START | (WHITE_START >> 16))
+        let (opp_start, opp_side, self_base) = match node.to_play {
+            Player::White => (
+                BLACK_START,
+                BLACK_START | (BLACK_START << 16),
+                WHITE_FIRST_ROW,
+            ),
+            Player::Black => (
+                WHITE_START,
+                WHITE_START | (WHITE_START >> 16),
+                BLACK_FIRST_ROW,
+            ),
         };
         if target_square & opp_start > 0 {
             // Priority 0: Almost-winning moves
@@ -81,8 +97,11 @@ fn get_prioritized_actions(node: &BreakthroughNode) -> Vec<BreakthroughMove> {
         } else if target_square & opp_side > 0 {
             // Priority 2: Entering the opponent's side of the board
             2
+        } else if ((1 << action.0) & self_base) > 0 {
+            // Avoid moving pieces from the start row unless necessary
+            100
         } else {
-            3
+            50
         }
     });
 
@@ -119,5 +138,10 @@ fn negamax(node: &BreakthroughNode, depth: u32, alpha: Evaluation, beta: Evaluat
 }
 
 pub fn evaluate(node: &BreakthroughNode, depth: u32) -> Evaluation {
-    negamax(node, depth, Evaluation::BlackWinPly(node.ply), Evaluation::WhiteWinPly(node.ply))
+    negamax(
+        node,
+        depth,
+        Evaluation::BlackWinPly(node.ply),
+        Evaluation::WhiteWinPly(node.ply),
+    )
 }
