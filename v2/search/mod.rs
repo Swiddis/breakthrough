929c760a8c1d39c99ff2d8186669d018524181ac
Simeon Widdis <sawiddis@gmail.com>
2023-09-30 11:32:23 -0700

    Add losing-move filtering

--- v2/search/mod.rs
+++ v2/search/mod.rs
@@ -43,7 +43,48 @@ fn fast_win(node: &BreakthroughNode) -> Option<Evaluation> {
 
 // Moves sorted by priority and filtered to remove obvious losing moves
 fn get_prioritized_actions(node: &BreakthroughNode) -> Vec<BreakthroughMove> {
-    node.get_possible_actions()
+    // We detect captures lazily, a destination on any piece is a capture
+    let any_bitboard = node.bitboard_white | node.bitboard_black;
+
+    let actions = node.get_possible_actions();
+
+    let mut actions: Vec<BreakthroughMove> = actions.into_iter().filter(|action| {
+        // If opponent is threatening a win, we can only recapture
+        match node.to_play {
+            Player::White => if node.bitboard_black & WHITE_START == 0 {
+                true
+            } else {
+                (1 << action.1) & node.bitboard_black & WHITE_START > 0
+            },
+            Player::Black => if node.bitboard_white & BLACK_START == 0 {
+                true
+            } else {
+                (1 << action.1) & node.bitboard_white & BLACK_START > 0 
+            }
+        }
+    }).collect();
+
+    actions.sort_unstable_by_key(|action| {
+        let target_square = 1 << action.1;
+        let (opp_start, opp_side) = match node.to_play {
+            Player::White => (BLACK_START, BLACK_START | (BLACK_START << 16)),
+            Player::Black => (WHITE_START, WHITE_START | (WHITE_START >> 16))
+        };
+        if target_square & opp_start > 0 {
+            // Priority 0: Almost-winning moves
+            0
+        } else if target_square & any_bitboard > 0 {
+            // Priority 1: Captures
+            1
+        } else if target_square & opp_side > 0 {
+            // Priority 2: Entering the opponent's side of the board
+            2
+        } else {
+            3
+        }
+    });
+
+    actions
 }
 
 fn negamax(node: &BreakthroughNode, depth: u32, alpha: Evaluation, beta: Evaluation) -> Evaluation {
@@ -54,9 +95,15 @@ fn negamax(node: &BreakthroughNode, depth: u32, alpha: Evaluation, beta: Evaluat
         return eval;
     }
 
+    let actions = get_prioritized_actions(node);
+    if actions.len() == 0 {
+        // If there's no reasonable actions, the opponent wins in the next turn
+        // Add 2 since lose state is on our next turn
+        return Evaluation::BlackWinPly(node.ply + 2);
+    }
+
     let (mut alpha, beta) = (alpha, beta);
     let mut value = Evaluation::BlackWinPly(node.ply);
-    let actions = get_prioritized_actions(node);
     for action in actions.iter() {
         let child = node.take_action(action);
         value = max(value, -negamax(&child, depth - 1, -beta, -alpha));
